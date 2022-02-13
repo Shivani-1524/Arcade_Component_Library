005f86bf157fe9fde0fc0a1bdd68fc5642e0e4b9
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-02-13 18:45:27 +0530

    fix: button height

--- Components/Button/button.js
+++ Components/Button/button.js
@@ -2,6 +2,7 @@ const starToggleBtn = document.getElementById("star-toggle-btn");
 
 starToggleBtn.setAttribute("style", "color: var(--light-grey)")
 starToggleBtn.addEventListener("click", () => {
+    console.log("MEOW")
     if (starToggleBtn.style.color === "var(--yellow-color)") {
         starToggleBtn.setAttribute("style", "color: var(--light-grey)")
     }
