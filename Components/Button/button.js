11d51e63749e57a22eed9674b13e848671e90f75
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-02-13 17:55:56 +0530

    add: badge-card , toggle button

---
+++ Components/Button/button.js
@@ -0,0 +1,11 @@
+const starToggleBtn = document.getElementById("star-toggle-btn");
+
+starToggleBtn.setAttribute("style", "color: var(--light-grey)")
+starToggleBtn.addEventListener("click", () => {
+    if (starToggleBtn.style.color === "var(--yellow-color)") {
+        starToggleBtn.setAttribute("style", "color: var(--light-grey)")
+    }
+    else {
+        starToggleBtn.setAttribute("style", "color: var(--yellow-color)")
+    }
+})
